5d938a861cc59e471dd0c52f98317a18c4107546
MemoGT <guillermo.sierra@gmail.com>
2022-07-14 12:07:48 -0600

    Hola Mundo

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,4 @@
-print ("HOla Mundo, esta es una bonita prueba")
+print ("Hola Mundo, esta es una bonita prueba")
+print ("Mi nombre es Guillermo")
+print ("El nombre de mi hijo es Alejandro \nEl nombre de mi hija es Diana")
+print ("El nombre de mi esposa es Claudia \nTerminamos")
