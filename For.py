a0afa4ba7245a6571053f9a7cd4bcd40f215989f
MemoGT <guillermo.sierra@gmail.com>
2022-09-06 05:56:46 -0600

    For-1

---
+++ For.py
@@ -0,0 +1,19 @@
+lista = ("Hola","Como","estas")
+for i in lista:
+    print(i)
+    print (len(lista))
+print ("")
+for i in range(1,11):
+    print(i);
+print ("")
+#Inicio, fin, salto
+for i in range (1,13,2):
+    print(i)
+print ("")
+for i in range (2,12,2):
+    print(i);
+print ("")
+for i in ange (10,0,-1):
+    print(i);
+print ("")
+
